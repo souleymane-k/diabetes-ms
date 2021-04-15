e9595a6d8543537ec841c4741e85e0cfabeb695e
souleymane kone <desolo74@gmail.com>
2021-04-15 16:49:45 -0700

    add api

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line 
 export default {
-    API_ENDPOINT : 'https://shrouded-scrubland-37554.herokuapp.com//api',
+    API_ENDPOINT : 'https://shrouded-scrubland-37554.herokuapp.com/api',
     API_KEY: process.env.NODE_ENV
 }
