46efa45ec703db67355260410cfa9e09087a76b7
souleymane kone <desolo74@gmail.com>
2021-05-11 06:17:21 -0700

    homepage

--- src/routes/Result/Result.js
+++ src/routes/Result/Result.js
@@ -57,21 +57,76 @@ export default class Result extends React.Component {
           {' '}
           Delete
         </button>
-        <div className='Result__dates'>
-          <div className='Result__dates-date_tested'>
-          Date_tested
+        <div className='Result__month_taken'>
+          <div className='Result__results_description'>
+          month_taken
+            {' '}
+            <span className='month_taken'>
+              {month_taken}
+            </span> 
+          </div> 
+         </div>
+         <div className='Result__meal_taken'>
+          <div className='Result__results_meal_taken'>
+          meal_taken
+            {' '}
+            <span className='meal_taken'>
+              {meal_taken}
+            </span> 
+          </div> 
+         </div>
+         <div className='Result__result_read'>
+          <div className='Result__dates_result_read'>
+          result_read
             {' '}
             <span className='Result'>
+              {result_read}
+            </span> 
+          </div> 
+         </div>
+         <div className='Result__result_dates'>
+          <div className='Result__result_date__tested'>
+          date__tested
+            {' '}
+            <span className='Result_results_date__tested'>
               {date_tested}
             </span> 
           </div> 
          </div>
+         <div className='Result__month_id'>
+          <div className='Result__results_month_id'>
+          month_id
+            {' '}
+            <span className='month_id'>
+              {month_id}
+            </span> 
+          </div> 
+         </div>
+         <div className='Result__userid'>
+          <div className='Result__results_userid'>
+          userid
+            {' '}
+            <span className='userid'>
+              {userid}
+            </span> 
+          </div> 
+         </div>
+        
          <div className='Result__description'>
           <div className='Result__results_description'>
           Description
             {' '}
             <span className='Description'>
               {description}
+            </span> 
+          </div> 
+         </div>
+         <div className='Result__diabetestype'>
+          <div className='Result__dates_diabetestype'>
+          diabetestype
+            {' '}
+            <span className='Result'>
+              {diabetestype}
             </span> 
           </div> 
          </div>
