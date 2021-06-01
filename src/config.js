2b8133f49424102192d0f18a57a17e5356975fa7
souleymane kone <desolo74@gmail.com>
2021-06-01 10:59:50 -0700

    final

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 // eslint-disable-next-line 
 export default {
-    // API_ENDPOINT : 'https://shrouded-scrubland-37554.herokuapp.com/api',
-     API_ENDPOINT: 'http://localhost:8001/api',
+    API_ENDPOINT : 'https://shrouded-scrubland-37554.herokuapp.com/api',
+    //  API_ENDPOINT: 'http://localhost:8001/api',
     API_TOKEN: process.env.NODE_ENV || '0d0e1dbb-8d52-49f0-9dbf-570ec4f5109a',
 }
